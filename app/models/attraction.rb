857171841c471485e360501139e85d08ed86e040
ethansagin <ethansagin@gmail.com>
2019-10-04 14:10:33 -0400

    remove uniqueness validation from attraction model

--- app/models/attraction.rb
+++ app/models/attraction.rb
@@ -1,7 +1,7 @@
 class Attraction < ApplicationRecord
     belongs_to :destination
 
-    validates :name, presence: true, uniqueness: true
+    validates :name, presence: true
 
     scope :recommended, -> { where(recommend: true).order('name asc') }
     scope :not_recommended, -> { where(recommend: false).order('name asc') } 
